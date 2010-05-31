6eacb194036ff1b666422d394928a4302226f829
crystalgold <me@crystalgold.fr>
2010-05-31 23:07:33 +0200

    Fixed some classes

--- include/functions.php
+++ include/functions.php
@@ -8,22 +8,27 @@
 	{
 		switch($jobId){
 			case 4060: return 5000; // Rune Knight
-			case 4063: return 5001; // Arch Bishop
 			case 4061: return 5002; // Warlock
-			case 4078: return 5003; // Genetic
-			case 4062: return 5004; // Ranger
+      case 4062: return 5004; // Ranger
+      case 4063: return 5001; // Arch Bishop
+			case 4064: return 5003; // Mechanic
 			case 4065: return 5005; // Guillotine Cross
 			
+			// 3rd Class (Regular to 3rd)
 			case 4073: return 5007; // Royal Guard
-			case 4077: return 5008; // Sura
 			case 4074: return 5009; // Sorcerer
-			case 4079: return 5010; // Shadow Chaser
-			case 4076: return 5010; // Wanderer ?
-			case 4075: return 5011; // Minstrel
-			case 4023: return 23; // Super novice
+      case 4075: return 5012; // Minstrel
+      case 4076: return 5010; // Wanderer ?
+      case 4077: return 5008; // Sura
+			case 4078: return 5011;
+      case 4079: return 5010; // Shadow Chaser
+      
+      case 4045: return 23;
 			
-			default: if ($jobId >= 4001 && $jobId < 4007)
-			          return $jobId - 4000;
+			default: if ($jobId >= 4001 && $jobId <= 4007)
+			          return $jobId - 4001; // High Novice / High 1st Class
+			         elseif ($jobId >= 4024 && $jobId < 4043)
+			          return $jobId - 4023; // 1st And 2nd Class Babys
 			         else
 			          return $jobId;
 		}
